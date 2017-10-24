af37b51cd27679f7afac27ff93b51a8057e60576
hugo <hugo@pc.da.sua.avo>
2017-10-24 20:51:52 -0200

    lulz

--- database/urls.py
+++ database/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'database'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.LogView.as_view(), name='log'),
+	url(r'^index/$', views.IndexView.as_view(), name = 'index'),
 ]
--- database/views.py
+++ database/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Materia
+from django.http import HttpResponseRedirect
+from django.views import generic
+from django.shortcuts import get_object_or_404, render
 
+class LogView(generic.ListView):
+	model = Materia
+	template_name = 'database/log.html'
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the database index.")
+class IndexView(generic.ListView):
+	model = Materia
+	template_name = 'database/index.html'
+def choose(request, codigo):
+	choice = Materia.get(pk = request.GET['choice'])
 
 # Create your views here.
