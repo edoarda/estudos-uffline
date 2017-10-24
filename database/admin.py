e7cac50da03dff1899714136c8e4ee8441cd6199
Edoarda <edvallim@gmail.com>
2017-10-24 14:27:28 -0200

    banco de dados bunitim

--- database/admin.py
+++ database/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Grupo
+from .models import Materia
+from .models import Periodo
+from .models import Recurso
+from .models import Foto
+from .models import URL
+
+admin.site.register(Grupo)
+admin.site.register(Materia)
+admin.site.register(Periodo)
+admin.site.register(Recurso)
+admin.site.register(Foto)
+admin.site.register(URL)
--- database/models.py
+++ database/models.py
@@ -1,37 +1,58 @@
 from django.db import models
 
+def upload_to(instance, filename):
+    return 'img/%s/%s' % (instance.user.user.id, filename)
+
 class Grupo(models.Model):
+    def __str__(self):
+        return self.materia_key
+
     t_inicio = models.DateTimeField('data e horário de inicio')
     t_fim = models.DateTimeField('data e horário de fim')
     n_total = models.PositiveSmallIntegerField(default=5)
     n_atual = models.PositiveSmallIntegerField(default=1)
-    local = models.Charfield(max_length=200)
-    materia = models.ForeignKey('Materia', on_delete=models.RESTRICT)
+    local = models.CharField(max_length=200)
+    materia_key = models.ForeignKey('Materia', on_delete=models.PROTECT)
 
 class Materia(models.Model):
     class Meta:
         unique_together = ('codigo', 'turma')
-    codigo = models.Charfield(max_length=8, primary_key=True)
-    turma = models.Charfield(max_length=2)
-    periodo = models.ForeignKey('Periodo', on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.nome
+
+    codigo = models.CharField(max_length=8, primary_key=True)
+    nome = models.CharField(max_length=40, default='me dê um nome de verdade')
+    turma = models.CharField(max_length=2)
+    periodo_key = models.ForeignKey('Periodo', on_delete=models.PROTECT)
 
 class Periodo(models.Model):
-    materia = models.ForeignKey('Materia', on_delete=models.RESTRICT)
-    periodo = models.PositiveSmallIntegerField(primary_key=True)
+    def __str__(self):
+        return self.periodo_nome
 
-class Recursos(model.Model):
-    nome = models.Charfield(max_length=80)
+    periodo_nome = models.CharField(max_length=6,default='2000.1')
+    periodo = models.PositiveIntegerField(primary_key=True)
+
+class Recurso(models.Model):
+    def __str__(self):
+        return self.nome
+
+    nome = models.CharField(max_length=80)
     descricao = models.CharField(max_length=500)
 
-class Fotos(model.Model):
-    recurso_id = models.ForeignKey('Recursos', on_delete=models.CASCADE)
-    foto = models.ImageField(upload_to=recurso_id + '/')
+class Foto(models.Model):
 
-class URLs(model.Models):
-    recurso_id = models.ForeignKey('Recursos', on_delete=models.CASCADE)
-    url = models.UrlField('')
+    recurso_id = models.ForeignKey('Recurso', on_delete=models.PROTECT)
+    foto = models.ImageField(upload_to=upload_to)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class URL(models.Model):
+    def __str__(self):
+        return self.url
+
+    recurso_id = models.ForeignKey('Recurso', on_delete=models.PROTECT)
+    url = models.URLField('')
+
+#class Choice(models.Model):
+#    question = models.ForeignKey(Question, on_delete=models.PROTECT)
+#    choice_text = models.CharField(max_length=200)
+#    votes = models.IntegerField(default=0)
