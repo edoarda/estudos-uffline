e43adf669782d2547b2962119c8447b19f114ef8
edoarda <edvallim@gmail.com>
2017-10-25 04:59:18 -0200

    peido

--- database/views.py
+++ database/views.py
@@ -15,6 +15,15 @@ class IndexView(generic.ListView):
 class MenuMateriaView(generic.ListView):
 	model = Materia
 	template_name = 'database/menumateria.html'
+	all_models_dict = {
+        "template_name": "contacts/index.html",
+        "queryset": Individual.objects.all(),
+        "extra_context" : {"grupo_list" : Grupo.objects.all(),
+                           "dataprova_list": DataProva.objects.all(),
+						   "material_list": Material.object.all(),
+                           #and so on for all the desired models...
+                           }
+    }
 
 #def menumateria(request, materia_codigo):
 #	choice = get_object_or_404(Materia, Materia.objects.filter(codigo=materia_codigo))
