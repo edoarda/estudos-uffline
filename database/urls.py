7251249586638875c2f984ee17a190395911de43
hugo <hugo@pc.da.sua.avo>
2017-10-25 00:52:34 -0200

    OIIIKARA

--- database/urls.py
+++ database/urls.py
@@ -6,5 +6,5 @@ app_name = 'database'
 urlpatterns = [
     url(r'^$', views.LogView.as_view(), name='log'),
 	url(r'^index/$', views.IndexView.as_view(), name = 'index'),
-	url(r'^materia(<materia_codigo>[A-Z0-9a-z]+)/$', views.MenuMateriaView.as_view(), name = menumateria'),
+	url(r'^materia(?P<materia_codigo>[A-Z0-9a-z]+)/$', views.MenuMateriaView.as_view(), name = 'menumateria'),
 ]
